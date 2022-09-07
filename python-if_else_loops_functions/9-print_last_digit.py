508da903012b2d83d229e60b4c9321dbe910814c
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-09-07 13:13:11 +0200

    Add 9 - Print the last digit of a number

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    "Prints the last digit of a number"
+    if number < 0:
+        lastdigit = (number * -1) % 10
+    else:
+        lastdigit = number % 10
+    
+    print(lastdigit, end='')
+
+    return lastdigit
