a9206e0be1042358baefbee8bc69d9abbb472d83
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-09-14 12:17:48 +0200

    12 - Correctt strlen if no string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,8 @@ def roman_to_int(roman_string):
     "Converts a Roman numeral to an integer"
     result = 0
     numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    strlen = len(roman_string)
     if roman_string and isinstance(roman_string, str):
+        strlen = len(roman_string)
         for i, s in enumerate(roman_string):
             raw_num = numbers[s]
             if i < strlen - 1 and raw_num < numbers[roman_string[i + 1]]:
