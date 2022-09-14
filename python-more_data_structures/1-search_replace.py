c4978a44689be1c10d64e2251698632f40ba9f24
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-09-14 11:02:10 +0200

    Add 1 - Replace all occurences of an element by another in a list

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    "Replaces all occurences of an element by another in a new list"
+    result = None
+    if my_list:
+        result = [replace if s == search else s for s in my_list]
+    return result
