691e1b8bfb7c6e035065d417e2ba8abd1a1e3469
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-09-16 12:49:56 +0200

    Correct 0 for 0 element case

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        if x == 0:
+            return 0
         for i, e in enumerate(my_list):
             print(e, end='')
             if i == x - 1:
