5e25f26bb25c165c4fc253ba18247e1474d7d7ef
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-09-16 12:43:33 +0200

    0 - Correct path

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,9 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i, e in enumerate(my_list):
-            if i == x:
-                break
             print(e, end='')
+            if i == x - 1:
+                break
         print("")
         return i + 1
     except ValueError as err:
