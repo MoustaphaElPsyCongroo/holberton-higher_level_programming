805bebffc95a6991bf97578eaac797faa078d33d
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-09-28 13:07:04 +0200

    Correct 0 documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+""""
+Write a function that returns the list of available attributes and methods of
+an object:
+
+Prototype: def lookup(obj):
+Returns a list object
+You are not allowed to import any module
+"""
+
+
 def lookup(obj):
+    """Lists available attributes and methods of an object"""
     return dir(obj)
