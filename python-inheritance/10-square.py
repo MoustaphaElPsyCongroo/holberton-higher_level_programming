e9f4eb3df3270a47a0c189b60f354d7aa68e0309
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-09-29 15:00:13 +0200

    Add 10 - Create Square, that inherits from Rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"Rectangle class."
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        "Get the square's area"
+        return (self.__size * self.__size)
