05f5dcf2d6c622c52ae567a5375b3849239a465e
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-09-30 23:27:19 +0200

    Add 7 - Save all arguments of a script to a file as JSON

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"Saves all arguments of this script to a file"
+from sys import argv
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = argv[1:]
+
+previous_cnts = load_from_json_file('add_item.json')
+tosav = [*previous_cnts, *args]
+save_to_json_file(tosav, "add_item.json")
