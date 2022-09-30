79c8e7414e24e70d5d36d5185ab2cc1ae1ce9a8f
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-09-30 22:14:37 +0200

    0 - add documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"Read a file"
+
+
 def read_file(filename=""):
+    "Reads a text file and prints it to stdout"
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read())
