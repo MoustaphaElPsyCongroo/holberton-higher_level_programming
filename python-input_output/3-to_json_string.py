0ddc6c0c2c645eed8b3f6eaa0b2efc6db74fc95a
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-09-30 22:36:27 +0200

    Add 3 - Get the JSON representation of an object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"Gets the JSON representation of an object"
+import json
+
+
+def to_json_string(my_obj):
+    "Gets the JSON representation of an object"
+    return json.dumps(my_obj)
