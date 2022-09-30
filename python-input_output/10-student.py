58390797b9de833d57ea0ada3e7c32d197c48ba9
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-09-30 23:54:27 +0200

    Add 10 - Get all or part of dic rep of a Student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"A Student class"
+
+
+class Student:
+    "Student class"
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Gets all or part of the dictionary representation of a Student
+        instance
+        """
+        if isinstance(attrs, list) and all(isinstance(s, str) for s in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
