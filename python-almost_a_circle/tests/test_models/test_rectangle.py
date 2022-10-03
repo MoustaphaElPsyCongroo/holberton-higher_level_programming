f580597aa439eda30f44e85207af48aa9fd7280d
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-10-03 12:08:31 +0200

    Add 2 - Add validation of all setters for Rectangle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -8,12 +8,30 @@ class TestRectangle(unittest.TestCase):
     "Unit test suite for Rectangle class"
 
     def test_init(self):
-        "test of Rectangle for width/height and default initialization"
+        "Test of Rectangle for width/height and default initialization"
         r = Rectangle(1, 2)
         self.assertEqual(r.width, 1)
         self.assertEqual(r.height, 2)
         self.assertEqual(r.x, 0)
         self.assertEqual(r.y, 0)
+        r = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(r.id, 5)
+
+    def test_type(self):
+        "Test of Rectangle for wrong attribute types"
+        self.assertRaises(TypeError, Rectangle, "1", 2)
+        self.assertRaises(TypeError, Rectangle, 1, "2")
+        self.assertRaises(TypeError, Rectangle, 1, 2, "3")
+        self.assertRaises(TypeError, Rectangle, 1, 2, 3, "4")
+
+    def test_value(self):
+        "Test of Rectangle for invalid values"
+        self.assertRaises(ValueError, Rectangle, -1, 2)
+        self.assertRaises(ValueError, Rectangle, 1, -2)
+        self.assertRaises(ValueError, Rectangle, 0, 2)
+        self.assertRaises(ValueError, Rectangle, 1, 0)
+        self.assertRaises(ValueError, Rectangle, 1, 2, -3)
+        self.assertRaises(ValueError, Rectangle, 1, 2, 3, -4)
 
 
 if __name__ == "__main__":
