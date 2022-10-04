f0d2ebc4d91060468fcf414cdb06216fdd9c1f0c
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-10-04 23:55:42 +0200

    Add 9 - Add Square class, subclass of Rectangle

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"Square class, subclass of Rectangle"
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    "Square class, subclass of Rectangle"
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width)
