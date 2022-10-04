44ce5da9412799e812fbfa9d9b45c19abc6bb99b
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-10-05 00:05:55 +0200

    Add 10 - Add size attribute and getter setters for Square class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,9 +8,19 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
                                                          self.x,
                                                          self.y,
                                                          self.width)
+
+    @property
+    def size(self):
+        "Getter/setter for size attribute"
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = self.height = value
