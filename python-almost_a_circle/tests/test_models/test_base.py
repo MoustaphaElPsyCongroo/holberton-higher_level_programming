743cd721e127306c6d9c57c47ffea9089d273ee1
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-10-05 12:17:23 +0200

    Add Base.to_json_string() tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -22,6 +22,26 @@ class TestBase(unittest.TestCase):
         b = Base("string")
         self.assertEqual(b.id, "string")
 
+    def test_to_json_string(self):
+        """ Test to_json_string """
+        dic = {'id': 1, 'x': 2, 'y': 3, 'width': 4, 'height': 5}
+        json_dic = Base.to_json_string([dic])
+        self.assertTrue(isinstance(dic, dict))
+        self.assertTrue(isinstance(json_dic, str))
+        self.assertCountEqual(
+            json_dic, '{["id": 1, "x": 2, "y": 3, "width": 4, "height": 5]}')
+        json_d_1 = Base.to_json_string(None)
+        self.assertEqual(json_d_1, "[]")
+        err = ("to_json_string() missing 1 required positional argument: " +
+               "'list_dictionaries'")
+        with self.assertRaises(TypeError) as i:
+            Base.to_json_string()
+        self.assertEqual(err, str(i.exception))
+        err = "to_json_string() takes 1 positional argument but 2 were given"
+        with self.assertRaises(TypeError) as i:
+            Base.to_json_string([{1, 2}], [{3, 4}])
+        self.assertEqual(err, str(i.exception))
+
 
 if __name__ == "__main__":
     unittest.main()
