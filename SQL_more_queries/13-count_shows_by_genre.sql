ee0c7198ba222876b77acf379d461eda66b92ba9
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-11-08 11:47:43 +0100

    13 - exclude genres with no shows

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,7 +6,7 @@ FROM
 	tv_genres
 	LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE
-	tv_show_genres.genre_id IS NOT NULL
+	tv_show_genres.show_id IS NOT NULL
 GROUP BY
 	tv_genres.name
 ORDER BY
