85b58f9844c211e3b653b0af5601c89f94737c4f
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-11-09 22:48:20 +0100

    1 - List all states with name starting with 'N'

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"Lists all states with a name starting with N"
+
+from sys import argv
+import MySQLdb
+
+user = argv[1]
+passwd = argv[2]
+datab = argv[3]
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, passwd=passwd, db=datab)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    results = cur.fetchall()
+
+    for result in results:
+        print(result)
+
+    cur.close()
+    db.close()
