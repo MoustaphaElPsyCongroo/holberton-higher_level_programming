5766b5f6f4d06770c4d25b5931b5e13b5a9504d1
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-11-09 23:58:04 +0100

    5 - Lists all cities of input state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"Lists all cities of input state"
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    datab = argv[3]
+    state = argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, passwd=passwd, db=datab)
+    cur = db.cursor()
+
+    cur.execute(
+        """SELECT cities.name FROM cities
+        INNER JOIN states ON cities.state_id = states.id
+        WHERE states.name LIKE BINARY %s
+        ORDER BY cities.id""", (state,))
+
+    results = cur.fetchall()
+    i = 0
+
+    for result in results:
+        for col in result:
+            if i == len(results) - 1:
+                print(col, end="")
+            else:
+                print(col, end=", ")
+            i += 1
+
+    cur.close()
+    db.close()
