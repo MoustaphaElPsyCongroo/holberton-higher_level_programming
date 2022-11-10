3320cb03625b8dac6f810db5faaf2b18bf045f30
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-11-10 18:43:34 +0100

    6 - Correct doc

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,11 +6,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 engine = create_engine(
-    'mysql+mysqldb://root:root@localhost:3306/hbtn_0e_4_usa', echo=True)
+    'mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa', echo=True)
 Base = declarative_base()
 
 
 class State(Base):
+    "State table model"
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, unique=True)
     name = Column(String(128), nullable=False)
