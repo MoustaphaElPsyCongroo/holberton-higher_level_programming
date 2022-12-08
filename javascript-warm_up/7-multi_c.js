f901e4cb5b1e46bed8004b7c62e71078b907662c
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-12-09 00:17:16 +0100

    7 - Use a traditional loop

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = process;
+const n = parseInt(argv[2]);
+
+if (isNaN(n)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < n; i++) {
+    console.log('C is fun');
+  }
+}
