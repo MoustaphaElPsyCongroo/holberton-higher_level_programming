4bb89d4fe315f3f3b49278fe872542e73fc4f475
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-12-12 17:19:33 +0100

    3 - Print the number of movies where Wedge Antilles is present

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+request(url, (error, response, body) => {
+  if (error) throw error;
+
+  const films = JSON.parse(body).results;
+  const wedgeFilms = films.filter(film => film.characters.includes('https://swapi-api.hbtn.io/api/people/18/'));
+  console.log(wedgeFilms.length);
+});
