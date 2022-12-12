e393b48f20e06dea7b96ccb50a61cf61c78a18ed
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-12-12 16:55:54 +0100

    1 - Write a string to a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const { argv } = process;
+const path = argv[2];
+const data = argv[3];
+
+fs.writeFile(path, data, 'utf8', err => {
+  if (err) throw err;
+});
